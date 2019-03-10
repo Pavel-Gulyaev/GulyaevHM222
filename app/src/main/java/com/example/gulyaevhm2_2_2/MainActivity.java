90a3d5aa60706609275a30483cce244133f4b1b7
Anna <panova2006_89@mail.ru>
2019-03-10 14:24:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gulyaevhm2_2_2/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.gulyaevhm2_2_2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu_main, menu);
+       return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+
+            Intent intentNotes = new Intent(this, NotesActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+        if (id == R.id.action_money) {
+
+            Intent intentNotes = new Intent(this, MoneyActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+        if (id == R.id.action_countries) {
+
+            Intent intentNotes = new Intent(this, CountriesActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+        if (id == R.id.action_dates) {
+
+            Intent intentNotes = new Intent(this, DatesActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
